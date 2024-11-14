6f48da6e2d6e66c2a50c3fee51cca95c40a80671
Harsh-Gehlot <81151468+Harsh-Gehlot@users.noreply.github.com>
2024-11-14 17:08:17 +0530

    reverse record

--- Dns.py
+++ Dns.py
@@ -6,30 +6,30 @@ import time
 # conf 1 zone at a time
 class BindSetup():
 
-    DB_REVERSE_RECORD=[]
-
-    def __init__(self, zone = "a10.networks.com", ip = "30.30.30.10", record = ["www    IN  A   127.0.0.1"]) -> None:
+    def __init__(self, zone = "a10.networks.com", ip = "30.30.20.10", record = ["www    IN  A   127.0.0.1"], revRecord = []) -> None:
 
         self.zone= zone
         self.IP_ADDRESS=ip
         self.prefix = "www"
         self.DB_RECORDS = record
+        self.DB_REVERSE_RECORD = revRecord
 
     def init(self, fw_record = [], rev_record = []):
         try:
             subprocess.run(["sudo", "apt", "install", "bind9"])
             time.sleep(10)
-            os.chdir("../etc/bind")
+            os.chdir("../")
+            os.chdir("/etc/bind")
             print(os.getcwd())
         except FileNotFoundError:
             print(f"Error : FileNotFound ")
         except Exception as e:
             print(f"Error: {str(e)}")
-        print("/etc/bind9" in os.getcwd())
-        if(True or "/etc/bind9" in os.getcwd() ):
-            print("copying original files")
-            subprocess.run(["sudo", "cp", "named.conf.options", "named.conf.options.orig"])
-            subprocess.run(["sudo", "cp", "named.conf.local", "named.conf.local.orig"])
+
+        if(True or "/etc/bind" in os.getcwd() ):
+            print(">copying original files...")
+            # subprocess.run(["sudo", "cp", "named.conf.options", "named.conf.options.orig"])
+            # subprocess.run(["sudo", "cp", "named.conf.local", "named.conf.local.orig"])
 
             with open("db."+self.zone, "w") as file:
                 file.write(self.Forward_Db_File())
@@ -37,28 +37,23 @@ class BindSetup():
             with open("named.conf.local", "a") as file:
                 file.write(self.Forward_Zone_file())
 
-            with open("named.conf.options", "a+") as file:
-                content = file.readlines()
-                print(content)
-               print( "\n \n \n", self.NamedOptions())
-        #     with open("db.domain", "w") as file:
-        #         self.dbRecord(ip="40.40.40.100")
-        #         self.dbRecord(ip="40.40.40.101")
-        #         file.write(createDBLocal())
-          
-    # def Forward_Db_File(self):
+            # fetch content of named.config.options
+            content =[]
+            with open("named.conf.options", "r") as file:
+                lines = file.readlines()
+                for x in range(len(lines)-1, 0, -1):
+                    content = lines[0:x]
+                    if "recursion" in lines[x]:
+                        break
+            # named.config.options        
+            with open("named.conf.options", "w") as file:
+                file.write(' '.join(content) + self.NamedOptions()+"\n }")
+            
+            # revers query
+            with open("db."+self.IP_ADDRESS, "w") as file:
+                file.write(self.Resverse_Db_File())
 
-    #     return f"$TTL    604800 \n \
-    #         @       IN      SOA     ns1.{self.zone}. root.{self.zone}. (\n \
-    #                                 2         ; Serial\n \
-    #                             604800         ; Refresh\n \
-    #                             86400         ; Retry\n \
-    #                             2419200         ; Expire\n \
-    #                             604800 )       ; Negative Cache TTL\n \
-    #     ;\n \
-    #     @       IN      NS      ns1.{self.zone}.\n \
-    #     {'\n'.join(self.DB_RECORDS)} \n\
-    #     @       IN      AAAA    ::1\n "
+            self.checkConfig()
 
     def Forward_Db_File(self):
         return '''$TTL    604800
@@ -77,28 +72,28 @@ class BindSetup():
     
     def Resverse_Db_File(self):
 
-        return '''; \n
-        ; BIND reverse data file for local loopback interface \n
-        ;\n
-        $TTL    604800\n 
-        @       IN      SOA     {zone}. root.{zone}. (\n 
-                                    1         ; Serial\n 
-                                604800         ; Refresh\n 
-                                86400         ; Retry\n 
-                                2419200         ; Expire\n 
-                                604800 )       ; Negative Cache TTL\n 
-        ;\n \
-        @       IN      NS      {zone}.\n 
-        {records} \
-        1.0.0   IN      PTR     localhost.\n
-        '''.format(zone=self.zone, records='\n'.join(self.DB_REVERSE_RECORD))
+        return ''';
+        ; BIND reverse data file for local loopback interface
+        ;
+        $TTL    604800
+        @       IN      SOA     {zone}. root.{zone}. (
+                                    1         ; Serial
+                                604800         ; Refresh
+                                86400         ; Retry
+                                2419200         ; Expire
+                                604800 )       ; Negative Cache TTL
+        ;
+        @       IN      NS      {zone}.
+        {records} 
+        1.0.0   IN      PTR     localhost.
+        '''.format(zone='.'.join(self.zone.split('.')[:-1]), records='\n'.join(self.DB_REVERSE_RECORD))
 
     def Forward_Zone_file(self):
-        return '''//forward lookup zone \n
-                zone \"{zone}\" IN { \n
-                type master; \n
-                file \"/etc/bind/db.ip\"; \n\
-        };\n'''.format(zone=self.zone)
+        return '''//forward lookup zone
+                zone \\"''' + self.zone +''' \\ IN {
+                type master; 
+                file \\"/etc/bind/db.ip\\";
+        };\n'''
     
     def NamedOptions(self):
         return '''recursion yes;
@@ -114,9 +109,8 @@ class BindSetup():
     def reverseDbRecord(self, ip, prefix ="www", ns = "A"):
         self.DB_RECORDS.append(f"{prefix}     IN      {ns}       {ip}")
 
+    def checkConfig(self):
 
-
-
-
-
-
+        subprocess.run(["named-checkconf"])
+        subprocess.run(["named-checkzone", f"{self.zone}",  f"db.{self.zone}"])
+        #named-checkzone 40.40.40.in-addr.arpa db.40.40.40

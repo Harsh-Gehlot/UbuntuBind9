90b74b7610e4c14258ece4d1eb366d124df6a979
Harsh-Gehlot <81151468+Harsh-Gehlot@users.noreply.github.com>
2024-11-14 17:09:59 +0530

    reverse record

--- RecordUtils.py
+++ RecordUtils.py
@@ -13,12 +13,20 @@ def filterArgs(args):
         for x in range(l, len(args.domain)):
             args.ipaddress.append(args.ipaddress[l-1])
 
-   
-if __name__ == "__main__": 
+def createRevRecords(args):
+    filterArgs(args)
+    return[ f"{args.ipaddress[x].split(".")[-1]}\tIN\tPTR\t{args.domain[x]+args.zone}" for x in range(0, len(args.domain))]
 
-    # print(args.domain)
-    # print(args.ipaddress)
-    # print(args.ns) 
-    # print(createRecords(args=None))
-    pass
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-d', '--domain', type=str, nargs='+', help="Domain")
+    parser.add_argument('-ip', '--ipaddress', type=str, nargs='+', help="IP Address")
+    parser.add_argument('-ns', '--ns', type=str, nargs='*', help="IP Address")
+    parser.add_argument('-z', '--zone', type=str, help="zone")
+
+    args = parser.parse_args() 
+    
     
